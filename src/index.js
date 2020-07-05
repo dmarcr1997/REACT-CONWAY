4927991126778e0da147600071e0ac34d132f67e
dmarc <dmarcr1997@gmail.com>
2020-07-05 15:48:09 -0500

    set up redux store

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
+import BlockReducer from './reducers/block_reducer';
 
-
+const store = createStore(BlockReducer, composeWithDevTools())
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
